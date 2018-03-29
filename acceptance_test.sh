79d56dfef01d263d86ad26f9936ed474607c52c3
soringhenta <33215534+soringhenta@users.noreply.github.com>
2018-03-29 10:35:59 +0300

    Update acceptance_test.sh

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+count=1
+while ! 2>/dev/null echo "" > /dev/tcp/localhost/8080
+do
+sleep 1
+((count++ > 60)) && {
+echo 'Eroare! Serviciul da timeout la pornire'
+exit 1
+}
+done
+
 test $(curl localhost:8080/sum?a=1\&b=2) -eq 3
 
